881e95ce8bbb7b775ec5ad12be390a2945dd2e39
smb11 <sgardilyan@gmail.com>
2020-03-14 02:43:07 +0400

    Updates

---
+++ precache-manifest.f96802cdaa64a13bc65bd3f5fa7e41f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1026b844e5c67293babe43341305fea",
+    "url": "/2048_React/index.html"
+  },
+  {
+    "revision": "4103fbd3adf67925cd6d",
+    "url": "/2048_React/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "3d9aa44410196e822c9a",
+    "url": "/2048_React/static/js/2.8e7ce1ab.chunk.js"
+  },
+  {
+    "revision": "e3c93b9224f3cb3fae92f605119cd3d9",
+    "url": "/2048_React/static/js/2.8e7ce1ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4103fbd3adf67925cd6d",
+    "url": "/2048_React/static/js/main.79ddd228.chunk.js"
+  },
+  {
+    "revision": "b80b5e997bac8131113c",
+    "url": "/2048_React/static/js/runtime-main.3bbe4192.js"
+  }
+]);
